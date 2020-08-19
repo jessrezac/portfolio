a9251deb8b3ba40999c8e201160da47a48568a45
Jess Rezac <jess.rezac@gmail.com>
2020-08-18 22:08:42 -0500

    Add sass functionality, set up file system, add data to config.json and info.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,47 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const config = require("./config.json")
+const infoData = require("./content/data/info.json")
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/content/posts`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: `${__dirname}/content/data`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "images",
+        path: `${__dirname}/content/images`,
+      },
+    },
+  ],
 }
