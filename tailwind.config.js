2f2be7e58455f619d62620300f8f26ba9ea865df
Jess Rezac <jess.rezac@gmail.com>
2020-08-18 22:55:55 -0500

    Configure fonts in Tailwind theme settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,11 @@ module.exports = {
       "morning-blue": "#84A59D",
       "light-coral": "#F28482",
     },
-    // fontFamily: {
-
-    // }
+    fontFamily: {
+      sans: [`Biryani`],
+      serif: [`Merriweather`],
+      display: [`Lora`],
+    },
   },
   variants: {},
   plugins: [],
