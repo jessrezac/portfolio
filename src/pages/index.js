8da3b7e45389bd01ff971f78e0d6f2c7d1027b63
Jess Rezac <jess.rezac@gmail.com>
2020-10-07 18:50:07 -0500

    Refactor index to move SignUpHero to component

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,28 +13,13 @@ import {
 import Layout from "../components/Layout"
 import BlogList from "../components/BlogList"
 import ProjectList from "../components/ProjectList"
-import SignUpForm from "./../components/SignUpForm"
-
-import headshot from "./../../content/images/jessrezac.jpg"
+import SignUpHero from "./../components/SignUpHero"
 
 export default function Home() {
   const { title, description, infoData } = useSiteMetadata()
   return (
     <Layout page="home">
-      <Helmet>
-        <html lang="en" />
-        <title>{title}</title>
-        <meta name="description" content={description} />
-      </Helmet>
-
-      <section className="bg-hero-collage bg-cover w-screen pb-20 flex flex-wrap-reverse items-center justify-center relative md:h-80vh">
-        <div className="bg-seashell w-4/5 md:w-2/4 p-10 rounded font-serif lg:z-10 relative lg:absolute lg:bottom-1/5 lg:left-1/10">
-          <SignUpForm />
-        </div>
-        <div className="w-4/5 md:w-1/3 text-center p-10 lg:z-0 relative lg:absolute lg:bottom-1/4 lg:right-15">
-          <img src={headshot} alt="" className="rounded shadowed" />
-        </div>
-      </section>
+      <SignUpHero />
 
       <ProjectList />
 
