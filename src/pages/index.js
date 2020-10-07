c767c44cd6562cade055a1b7a8faad2850256049
Jess Rezac <jess.rezac@gmail.com>
2020-10-07 18:56:07 -0500

    Refactor index to move SkillsList to component

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ import BlogList from "../components/BlogList"
 import ProjectList from "../components/ProjectList"
 import SignUpHero from "./../components/SignUpHero"
 import CallOutBanner from "./../components/CallOutBanner"
+import SkillsList from "./../components/SkillsList"
 
 export default function Home() {
   const { title, description, infoData } = useSiteMetadata()
@@ -28,44 +29,7 @@ export default function Home() {
 
       <CallOutBanner />
 
-      <section id="skills" className="w-screen bg-queen-blue text-seashell">
-        <div className="flex items-start justify-evenly p-10 text-center">
-          <div className="flex-1 p-2">
-            <div className="text-5xl">
-              <FontAwesomeIcon icon={faGem} />
-            </div>
-            <div className="text-2xl">Ruby</div>
-            <p>Ruby, Ruby on Rails, Sinatra, API Creation</p>
-          </div>
-          <div className="flex-1 p-2">
-            <div className="text-5xl">
-              <FontAwesomeIcon icon={faJs} />
-            </div>
-            <div className="text-2xl">Javascript</div>
-            <p>ES6, React, Redux, Gatsby, API use</p>
-          </div>
-          <div className="flex-1 p-2">
-            <div className="text-5xl">
-              <FontAwesomeIcon icon={faHtml5} />
-            </div>
-            <div className="text-2xl">HTML</div>
-            <p>Wordpress, HTML5</p>
-          </div>
-          <div className="flex-1 p-2">
-            <div className="text-5xl">
-              <FontAwesomeIcon icon={faCss3Alt} />
-            </div>
-            <div className="text-2xl">CSS</div>
-            <p>CSS3, Tailwind, Bulma, Bootstrap</p>
-          </div>
-          <div className="flex-1 p-2">
-            <div className="text-5xl">
-              <FontAwesomeIcon icon={faSwift} />
-            </div>
-            <div className="text-2xl">Swift</div>
-          </div>
-        </div>
-      </section>
+      <SkillsList />
 
       <footer className="bg-morning-blue h-100">Test</footer>
     </Layout>
