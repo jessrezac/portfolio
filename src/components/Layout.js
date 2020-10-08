f96c886a0f32c04f865dfc5bb6618cbd92c527fa
Jess Rezac <jess.rezac@gmail.com>
2020-10-07 19:08:44 -0500

    Add logic to title pages in Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,12 +5,15 @@ import Header from "./Header"
 
 function Layout(props) {
   const { title, description } = props
+  const siteTitle = useSiteMetadata().title
 
   return (
     <>
       <Helmet>
         <html lang="en" />
-        <title>{title}</title>
+        <title>
+          {props.page === "home" ? title : `${title} | ${siteTitle}`}
+        </title>
         <meta name="description" content={description} />
       </Helmet>
       <Header identity={props.page === "home" ? "large" : "small"} />
