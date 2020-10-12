9ec5868f5b34dc9895edff774eea99c46ed6e425
Jess Rezac <jess.rezac@gmail.com>
2020-10-11 21:06:19 -0500

    Use gatsby Link in Socials

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faLinkedinIn,
@@ -11,10 +12,18 @@ function Socials(props) {
   return (
     <>
       {" "}
-      <FontAwesomeIcon icon={faGithub} />
-      <FontAwesomeIcon icon={faLinkedinIn} />
-      <FontAwesomeIcon icon={faTwitter} />
-      <FontAwesomeIcon icon={faDev} />
+      <Link to="https://www.github.com/jessrezac" className="p-4">
+        <FontAwesomeIcon icon={faGithub} />
+      </Link>
+      <Link to="https://linkedin.com/in/jessrezac" className="p-4">
+        <FontAwesomeIcon icon={faLinkedinIn} />
+      </Link>
+      <Link to="https://twitter.com/cheerupemodev" className="p-4">
+        <FontAwesomeIcon icon={faTwitter} />
+      </Link>
+      <Link to="https://dev.to/cheerupemodev" className="p-4">
+        <FontAwesomeIcon icon={faDev} />
+      </Link>
     </>
   )
 }
