a15bc5d8a736f1950ae1311ed0806b464a9c1b25
Jess Rezac <jess.rezac@gmail.com>
2020-10-14 14:27:30 -0500

    Remove FontAwesome imports from SkillList component

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -3,15 +3,6 @@ import React from "react"
 import useSkillData from "../static_queries/useSkillData"
 import Skill from "./Skill"
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGem } from "@fortawesome/free-solid-svg-icons"
-import {
-  faJs,
-  faHtml5,
-  faCss3Alt,
-  faSwift,
-} from "@fortawesome/free-brands-svg-icons"
-
 function SkillsList(props) {
   let skills = useSkillData()
   console.log(skills)
