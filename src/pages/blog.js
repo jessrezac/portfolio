2da316763c660afcbc6aec2b264e124d543cc193
Jess Rezac <jess.rezac@gmail.com>
2020-10-23 19:00:42 -0500

    Add projects.json and blog.json to content; Import projectData and blogData to gatsby-config

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,8 +2,10 @@ import React from "react"
 import Layout from "./../components/Layout"
 import BlogList from "./../components/BlogList"
 import PageMast from "./../components/PageMast"
+import useSiteMetadata from "../static_queries/useSiteMetadata"
 
 function Blog(props) {
+  const { blogData } = useSiteMetadata()
   const colorClasses = "bg-morning-blue text-seashell"
   return (
     <Layout page="blog" title="Blog" colorClasses={colorClasses}>
