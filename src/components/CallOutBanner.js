5c6171f787746b911b9fffc3f9782d16ec0d9acc
Jess Rezac <jess.rezac@gmail.com>
2020-10-25 13:21:20 -0500

    Change background of callout banner

--- src/components/CallOutBanner.js
+++ src/components/CallOutBanner.js
@@ -1,9 +1,10 @@
 import React from "react"
+import paint from "./../../content/images/bluepaint.png"
 
 function CallOutBanner(props) {
   return (
-    <section className="bg-watercolor-texture bg-cover p-10 w-screen flex items-center justify-center">
-      <div className="w-4/5 md:w-2/4 p-10 text-center text-5xl font-display">
+    <section className="bg-baby-pink-100 bg-cover p-10 w-screen flex items-center justify-center">
+      <div className="bg-bluepaint bg-contain bg-center bg-no-repeat w-4/5 md:w-2/4 p-10 text-center text-5xl font-display">
         Take a<br />
         deep breath.
       </div>
